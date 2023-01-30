f0fc33a395c14daee2bdfc58bbaa9aa9fe367483
Adetola Adedeji <adetolaadedeji0@gmail.com>
2023-01-30 07:13:52 -0800

    Add like and delete icons to Post

--- src/components/PostComponent/Post/Post.js
+++ src/components/PostComponent/Post/Post.js
@@ -26,6 +26,23 @@ const Post = ({ post }) => {
                     {post.tags.map((tag) => `#{tag} `)}
                 </Typography>
            </div>
+
+           <CardContent>
+               <Typography className={classes.title} variant='h5' gutterBottom>{post.message}</Typography>
+          </CardContent>
+
+          <CardActions className={classes.cardActions}>
+               <Button size='small' color='primary' onClick={() => {}}>
+                    <ThumbUpAltIcon fontSize='small' />
+                    Like
+                    {post.likeCount}
+               </Button>
+
+               <Button size='small' color='primary' onClick={() => {}}>
+                    <DeleteIcon fontSize='small' />
+                    Delete
+               </Button>
+          </CardActions>
         </Card>
      );
 }
